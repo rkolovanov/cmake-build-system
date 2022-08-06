b999edfafe8546446233a3e8dcbcc3b78fa43fd2
rkolovanov <rodion.kolovanov@inbox.ru>
2022-08-06 23:32:46 +0300

    Поиск и подключение через find_package

--- sources/CMakeBuildSystem.cmake
+++ sources/CMakeBuildSystem.cmake
@@ -9,6 +9,6 @@ include(${CMAKE_BUILD_SYSTEM_DIR}/Definitions.cmake)
 include(${CMAKE_BUILD_SYSTEM_DIR}/Functions.cmake)
 include(${CMAKE_BUILD_SYSTEM_DIR}/Pack.cmake)
 
-message("Using CMakeBuildSystem version ${CMAKE_BUILD_SYSTEM_VERSION}")
+message("Using CMakeBuildSystem")
 message("Build type: ${CMAKE_BUILD_TYPE}")
 message("Platform: ${CBS_SYSTEM} ${CBS_ARCHITECTURE}")
